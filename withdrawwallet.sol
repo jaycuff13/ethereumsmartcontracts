02baa9408ea49358395444d4f462d9f1676781e8
jaycuff13 <48173061+jaycuff13@users.noreply.github.com>
2021-01-02 11:48:57 -0800

    Create withdrawwallet.sol

---
+++ withdrawwallet.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.26;
+
+contract Faucet {
+    function withdraw(uint withdraw_amount) public{
+        
+        require(withdraw_amount <= 10000000000000000);
+        
+        msg.sender.transfer(withdraw_amount);
+        
+    }
+    
+    
+    function () public payable{}
+}
+
